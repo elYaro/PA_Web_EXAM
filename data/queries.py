e0ebb243dd05ba85c81250aad8ba6243181f17bf
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-08-20 14:23:08 +0200

    merging commit

--- data/queries.py
+++ data/queries.py
@@ -4,7 +4,6 @@ from data import data_manager
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
-<<<<<<< HEAD
 #1 Yaro 
 def get_tv_show(tv_show_id):
     return data_manager.execute_select('SELECT * FROM shows WHERE id = %(id)s;', {'id': tv_show_id})
@@ -28,11 +27,9 @@ def get_total_seasons_number(tv_show_id):
     return data_manager.execute_select('''   SELECT se.season_number, se.title FROM seasons se
                                             JOIN shows sh ON se.show_id = sh.id
                                             WHERE sh.id = %(id)s AND se.season_number >=0;''', {'id'  : tv_show_id })
-=======
 
 def get_episodes(season_id):
     return data_manager.execute_select('''  SELECT ep.id AS episode_id, ep.title As episode_title, ep.episode_number, ep.overview, ep.season_id, se.title AS season_title, sh.id AS show_id, sh.title AS show_title FROM episodes ep
                                             JOIN seasons se ON ep.season_id = se.id
                                             JOIN shows sh ON sh.id = se.show_id
                                             WHERE se.id = %(id)s;''', {'id' : season_id })
->>>>>>> episodes
